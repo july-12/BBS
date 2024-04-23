430d751b7eeaacd84c8ca5b68c3552f1c7b61c57
july-12 <seventanyb@gmail.com>
2024-04-23 11:46:25 +0800

    feat: profile dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :comment, :favorite]
+
   def index
+    @posts = current_user.posts.order(updated_at: :desc)
   end
 
   def comment
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,5 @@
 module DashboardHelper
   def get_class_name(active)
-    active ? "inline-block p-3 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500" : "inline-block p-3 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+    active ? "inline-block p-3 text-blue-600 bg-gray-100 rounded-t-lg bg-white active dark:bg-gray-800 dark:text-blue-500" : "inline-block p-3 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
   end
 end
