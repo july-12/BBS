256ec7304b572bd715b164613bc76fe8be8e5e58
july-12 <seventanyb@gmail.com>
2024-04-27 22:36:06 +0800

    feat: comment replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,14 @@ class Comment < ApplicationRecord
   belongs_to :commenter, class_name: :User, foreign_key: :author_id
   belongs_to :commentable, polymorphic: true
 
+  has_many :replies, class_name: "Comment", foreign_key: :reply_id
+
   validates :author_id, presence: true
   validates :content, presence: true, length: { minimum: 2 }
 
-  default_scope -> { where("commentable_type IS NOT NULL") }
+  default_scope -> { where("commentable_type IS NOT NULL").order(created_at: "ASC") }
+
+  scope :no_reply_comments, -> { where("reply_id IS NULL") }
 
   after_create_commit -> { broadcast_append_to commentable }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: :User
   has_many :comments, as: :commentable, dependent: :destroy
+  # has_many :comments, -> { no_reply_comments }, as: :commentable, dependent: :destroy
 
   validates :title, presence: true
   validates :author_id, presence: true
