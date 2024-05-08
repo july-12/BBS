3179926f7008ef3c0c664c354329ad3dd7df4608
july-12 <seventanyb@gmail.com>
2024-05-08 19:21:21 +0800

    feat: wiki & sites

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all.order(created_at: "desc")
   end
+
+  def wiki
+  end
+
+  def sites
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "wiki", to: "home#wiki"
+  get "sites", to: "home#sites"
   devise_for :users
 
   get "setting", to: "users#setting"
